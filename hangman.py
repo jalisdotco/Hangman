10cbaebd37282196ebddcf6b2b9b4b58c7ff4ede
Boy, The <m1n1m4rsh1v4@gmail.com>
2017-01-25 15:05:53 -0800

    Initial Upload

---
+++ hangman.py
@@ -0,0 +1,25 @@
+import random, string, sys 
+
+bank = ['print','bank','prints','printing','printed','printer','letter',\
+'kalashkinov','honduras','yeti','roaming','backup','ohkay']
+bank = ['hello there']
+letters = string.ascii_lowercase
+words = random.choice(bank)
+##star = '*' * len(words)            
+guesses = 0
+end = 'game over'
+
+#
+print words
+#
+print guesses
+##print star
+while guesses != 10: 
+    command = raw_input("Your guess:")
+    if command in ['quit','exit']:
+        sys.exit(0)
+    guesses += 1
+
+    print guesses
+
+    
